5a515cf705ab0e52010f7bda0c6f047c3df1c155
firstthing2do <atgpro100@gmail.com>
2024-08-15 23:26:01 +0000

    Worked and finished the basic backend for adding, listing and removing both album and songs and details attached to them in their particular models and controllers

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import songRouter from './src/routes/songRouter.js'
+import songRouter from './src/routes/songRoute.js'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import albumRouter from './src/routes/albumRoute.js'
 
 // app config
 
@@ -20,6 +21,7 @@ app.use(cors())
 //init routes
 
 app.use('/api/song', songRouter)
+app.use('/api/album', albumRouter)
 
 app.get('/', (req, res) => res.send('API working!!'))
 app.listen(port,() => console.log(`Server running on port number ${port}`))
--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary} from 'cloudinary'
 import songModel from '../models/songModel.js'
+
 const addSong = async (req, res) => {
 
    try {
@@ -10,22 +11,48 @@ const addSong = async (req, res) => {
     const imageFile = req.files.image[0]
     const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video"})
     const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+    const duration = `${Math.floor(audioUpload.duration/60)}:${Math.floor(audioUpload.duration%60)}`
 
     const songData = {
         name,
         desc,
         album,
         image:imageUpload.secure_url,
-        file: audioUpload.secure_url
+        file: audioUpload.secure_url,
+        duration
     }
 
+    // to save the data into the database
+
+    const song = songModel(songData)
+    await song.save()
+
+    res.json({success: true, message: "Song Added"})
+
    } catch (error) {
-    
+    res.json({success: false})
    }
 }
 
 const listSong = async (req, res) => {
-
+    //getting songs data in one array
+    try {
+        const allSongs = await songModel.find({})
+        res.json({success:true, songs: allSongs})
+    } catch (error) {
+        res.json({success:false})
+    }
 }
 
-export {addSong, listSong}
+// remove song api
+
+const removeSong = async(req, res) =>{
+    try {
+        await songModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message:"Song removed"})
+    } catch (error) {
+        res.json({success:false})
+    }
+} 
+
+export {addSong, listSong, removeSong}
