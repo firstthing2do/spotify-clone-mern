e19fd3efb08fe0dcaf9278c4e3975caaa3d427e3
firstthing2do <atgpro100@gmail.com>
2024-08-21 00:07:15 +0000

    Worked and finished the functionalities of adding and listing songs to and from the database

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -7,14 +7,16 @@ import AddAlbum from './pages/addAlbum'
 import ListSong from './pages/ListSong'
 import ListAlbum from './pages/listAlbum'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 
+export const url = 'http://localhost:4000'
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer />
       <Sidebar />
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
-
+      <Navbar />
         <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
 
           <Routes>
--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -1,10 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import axios from 'axios'
+import { url } from '../App'
+import { toast } from 'react-toastify'
 
 const AddSong = () => {
-  return (
-    <div>
-      
+
+  const [image, setImage] = useState(false)
+  const [song, setSong] = useState(false)
+  const [name, setName] = useState("")
+  const [desc, setDesc] = useState("")
+  const [album, setAlbum] = useState("none")
+  const [loading, setLoading] = useState(false)
+  const [albumData, setAlbumData] = useState([])
+
+  const submitHandler = async (e) =>{
+    e.preventDefault()
+    setLoading(true)
+    try {
+      const formData = new FormData()
+
+      formData.append('name', name)
+      formData.append('desc', desc)
+      formData.append('image', image)
+      formData.append('audio', song)
+      formData.append('album', album)
+
+
+      const response = await axios.post(`${url}/api/song/add`, formData)
+
+      if (response.data.success) {
+         toast.success("Song Added")
+         setName("")
+         setDesc("")
+         setAlbum("none")
+         setImage(false)
+         setSong(false)
+      }
+
+      else{
+        toast.error("Something went wrong")
+      }
+
+    } catch (error) {
+      toast.error("Error occured")
+    }
+    setLoading(false)
+  }
+  return loading ? (
+    <div className='grid place-items-center min-h-[80vh]'>
+      <div className='w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin'></div>
     </div>
+  ) : (
+    <form onSubmit={submitHandler} className='flex flex-col items-start gap-5 text-gray-600'>
+     <div className='flex gap-8'>
+      <div className='flex flex-col gap-4'>
+        <p>Upload Song</p>
+        <input onChange={(e) => setSong(e.target.files[0])} type="file" id='song' accept='audio/*' hidden />
+        <label htmlFor="song">
+          <img src={song ? assets.u_added : assets.u_song} className='w-24 cursor-pointer' alt="" />
+        </label>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <p>Upload Image</p>
+        <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' accept='song/*' hidden />
+        <label htmlFor="image">
+          <img src={image ? URL.createObjectURL(image) : assets.u_area} className='w-24 cursor-pointer' alt="" />
+        </label>
+      </div>
+     </div>
+
+     <div className='flex flex-col gap-2.5'>
+      <p>Song Name</p>
+      <input onChange={(e) => setName(e.target.value)} value={name} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' placeholder='Type here' type="text"  required />
+     </div>
+
+     <div className='flex flex-col gap-2.5'>
+      <p>Song Discription</p>
+      <input onChange={(e) => setDesc(e.target.value)} value={desc} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' placeholder='Type here' type="text"  required />
+     </div>
+
+     <div className='flex flex-col gap-2.5'>
+      <p>Album</p>
+      <select onChange={(e) => setAlbum(e.target.value)} defaultValue={album} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px] '>
+        <option value="none">None</option>
+      </select>
+     </div>
+
+     <button type='submit' className='text-base bg-black text-white py-2.5 px-14 cursor-pointer rounded'>Add</button>
+
+    </form>
   )
 }
 
